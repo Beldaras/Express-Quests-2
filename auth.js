cb104fe665b1dfdd90feda2bc0f5671e951fab66
Beldaras <jphilippe.roblot@gmail.com>
2023-02-20 11:50:03 +0100

    save challenge

---
+++ auth.js
@@ -0,0 +1,28 @@
+const argon2 = require('argon2');
+
+
+
+const hashPassword = (req, res, next) => { 
+argon2
+    .hash(req.body.password, {
+        type: argon2.argon2id,
+        memoryCost: 2**16,
+        timeCost: 5,
+        parallelism: 1,
+    })
+    .then((hashedPassword) => {
+        
+        req.body.hashedPassword = hashedPassword;
+        delete req.body.password;
+
+        next();
+    })
+    .catch((err) => {
+        console.err(err);
+        resizeBy.sendStatus(500);
+    });
+};
+
+module.exports = {
+    hashPassword
+};
