6f9acba3762c5df9e1d28ac6b4923ff3bce44799
Beldaras <jphilippe.roblot@gmail.com>
2023-02-23 11:51:01 +0100

    quest Express 8 done without bonus

--- app.js
+++ app.js
@@ -14,23 +14,30 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const {hashPassword, verifyPassword, verifyToken} = require("./auth");
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+
+// public routes
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const {hashPassword} = require("./auth");
-
-const userHandlers = require("./userHandlers");
-
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+
+
+// Protected routes
+app.use(verifyToken);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
 
 app.listen(port, (err) => {
   if (err) {
